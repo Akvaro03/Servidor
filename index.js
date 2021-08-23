e4e1182f98ec83a81054a25beffaffd03eca1f26
Akvaro03 <68175898+Akvaro03@users.noreply.github.com>
2021-08-23 12:16:47 -0300

    post

--- index.js
+++ index.js
@@ -42,17 +42,32 @@ app.use(express.static('./public'))
 app.use('/', require('./routesWeb'));
 app.get('/urlparam', async(req, res) => {
     const { a, b } = req.query;
-    res.status(200).send("hola");
-    try {
-        res.status(200);
-        if (a != undefined || b != undefined) {
-            const datos = await new Datos({ a, b });
-            await datos.save();
-        }
-    } catch (e) {
-        throw new Error(`Error guardando datos: ${e}`)
-    }
+    res.status(200).send("Funciono");
+    const datos = await new Datos({ a, b });
+    await datos.save();
 });
+
+app.post('/post', async function(req, res) {
+    res.send('POST request to the homepage')
+    const { a, b } = req.body;
+    const z = req.body.a;
+    const y = req.body.b;
+    console.log({ a, b })
+    console.log({ y, z })
+    const datos = await new Datos({ a, b });
+    await datos.save();
+    res.status(200).send("Funciono");
+
+    // try {
+    //     res.status(200);
+    //     if (a != undefined || b != undefined) {
+    //         const datos = await new Datos({ a, b });
+    //         await datos.save();
+    //     }
+    // } catch (e) {
+    //     throw new Error(`Error guardando datos: ${e}`)
+    // }
+})
 
 app.get('/cuenta', (req, res) => {
     res.sendFile(`${__dirname}/public/sesiones.html`);
